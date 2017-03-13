96e646a6008c0096cbe7aaeb7c72efd88a7efed7
Prophet Xu <slowfishcolor@gmail.com>
2017-03-13 20:57:24 +0800

    add task controller, not finish

---
+++ src/main/java/com/mist/cloudtestingplatform/web/TaskController.java
@@ -0,0 +1,11 @@
+package com.mist.cloudtestingplatform.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 任务页面相关
+ * Created by Prophet on 2017/3/12.
+ */
+@Controller
+public class TaskController {
+}
