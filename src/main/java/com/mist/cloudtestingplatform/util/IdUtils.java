37a531af444c826a6964a6824945ba6f2bca5991
prophet <slowfishcolor@gmail.com>
2017-11-05 22:37:09 +0800

    ADD: add IdUtils to generate ids

---
+++ src/main/java/com/mist/cloudtestingplatform/util/IdUtils.java
@@ -0,0 +1,20 @@
+package com.mist.cloudtestingplatform.util;
+
+import java.util.UUID;
+
+/**
+ * Created by Prophet on 2017/11/5.
+ */
+public class IdUtils {
+
+    public static String generateDeviceId() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            System.out.println(generateDeviceId());
+        }
+        System.out.println(generateDeviceId());
+    }
+}
