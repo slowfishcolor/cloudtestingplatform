a39b149e8e705f981f402cab63052247c8045222
prophet <slowfishcolor@gmail.com>
2017-11-05 22:14:17 +0800

    ADD: device-control-panel page, TODO: make this page functional

--- src/main/java/com/mist/cloudtestingplatform/web/DeviceController.java
+++ src/main/java/com/mist/cloudtestingplatform/web/DeviceController.java
@@ -1,8 +1,12 @@
 package com.mist.cloudtestingplatform.web;
 
+import com.mist.cloudtestingplatform.model.Device;
+import com.mist.cloudtestingplatform.service.DeviceService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,9 +20,18 @@ public class DeviceController {
 
     private static final Logger logger = LoggerFactory.getLogger(DeviceController.class);
 
-    @RequestMapping(value = "/device/{deviceId}", method = RequestMethod.GET)
-    public String controlPanelPage(@PathVariable("deviceId") Integer deviceId) {
+    private DeviceService deviceService;
+
+    @Autowired
+    public void setDeviceService(DeviceService deviceService) {
+        this.deviceService = deviceService;
+    }
+
+    @RequestMapping(value = "/device-control-panel/{deviceId}", method = RequestMethod.GET)
+    public String controlPanelPage(@PathVariable("deviceId") String deviceId, Model model) {
+        Device device = deviceService.getDeviceByDeviceId(deviceId);
+        model.addAttribute(device);
         logger.info("deviceId: " + deviceId);
-        return "device-control-panel";
+        return "device-control-panel-pcie6320";
     }
 }
