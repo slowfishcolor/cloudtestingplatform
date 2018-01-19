d53eb100320eda1b99d3129d947fb8ecf4e58e56
prophet <slowfishcolor@gmail.com>
2018-01-19 15:55:08 +0800

    ADD: add pcie6320 description

---
+++ src/main/webapp/resources/js/device-description.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Prophet on 2018/1/19.
+ */
+$(function() {
+    var extractor = new Xsd2Json("CMfgTestInstrument.xsd", {"schemaURI":"../resources/atml/"});
+
+    $("#xml_editor").xmlEditor({
+        ajaxOptions: {
+            xmlRetrievalPath: "../resources/atml/cmfg-pcie6320.xml"
+        },
+        elementUpdated: hideLoading,
+        confirmExitWhenUnsubmitted : false,
+        submitButtonConfigs : [
+            {
+                url : "/submit",
+//                                responseHandler : myResponseHandler,
+                label : "保存",
+                cssClass: "btn btn-primary mg-r mg-b mg-t"
+            },
+            {
+                label : "重置",
+                onSubmit : function() {
+                    // do work
+                },
+                cssClass: "btn btn-default mg-r mg-b mg-t"
+            }
+        ],
+        schema: extractor.getSchema()
+    });
+});
+function hideLoading() {
+    $(".loading").hide();
+}
